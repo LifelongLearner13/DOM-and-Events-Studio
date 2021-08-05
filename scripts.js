a33a0f8895f04e32111585467fa5420d61c1fb0d
LifelongLearner13 <sierragregg@gmail.com>
2021-08-05 18:50:37 -0500

    first commit

--- scripts.js
+++ scripts.js
@@ -1,2 +1,12 @@
-// Write your JavaScript code here.
-// Remember to pay attention to page loading!
+window.addEventListener('load', function () {
+  const flightStatus = document.getElementById('flightStatus');
+  const takeOffBtn = document.getElementById('takeoff');
+  takeOffBtn.addEventListener('click', function () {
+    const result = window.confirm(
+      'Are you sure the shuttle is ready for takeoff?'
+    );
+    if (result) {
+      flightStatus.innerHTML = 'Shuttle in flight!';
+    }
+  });
+});
